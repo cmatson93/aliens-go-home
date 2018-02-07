68c9fa82bae951ef4ef6886032b6fb15cb107311
Christina  Matson <cmatson93@gmail.com>
2018-02-06 22:40:19 -0800

    Canon movement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formula';
 import Canvas from './components/Canvas';
-// import Sky from "./components/Sky";
 
 class App extends Component {
-    render() {
-        return ( 
-          < Canvas / >
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.trackMouse = this.trackMouse.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+      self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
+  render() {
+    return (
+      <Canvas
+        angle={this.props.angle}
+        trackMouse={event => (this.trackMouse(event))}
+      />
+    );
+  }
 }
+
+App.propTypes = {
+  angle: PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired,
+};
 
 export default App;
