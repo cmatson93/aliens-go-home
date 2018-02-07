eeddd7e8338db07fa6fa8ae69e9bb3e820e806ca
Christina  Matson <cmatson93@gmail.com>
2018-02-06 22:26:24 -0800

    Cannon start

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Sky from '../Sky';
+import Ground from '../Ground';
+import CannonBase from "../CannonBase";
+import CannonPipe from "../CannonPipe";
 
 const Canvas = () => {
     const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
-    return ( <
-        svg id = "aliens-go-home-canvas"
-        preserveAspectRatio = "xMaxYMax none"
-        viewBox = { viewBox } >
-        <
-        Sky / >
-        <
-        circle cx = { 0 }
-        cy = { 0 }
-        r = { 50 }
-        /> <
-        /svg>
+    return (
+        <svg
+            id="aliens-go-home-canvas"
+            preserveAspectRatio="xMaxYMax none"
+            viewBox={viewBox}
+        >
+            <Sky />
+            <Ground /> 
+            <CannonPipe />
+            <CannonBase /> 
+        </svg>
     );
 };
 
